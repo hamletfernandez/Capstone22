3442ca706793a161688fa669a37aba5c6aaa87c9
Hamlet Fernandez <hamletfernandez57@gmail.com>
2022-03-09 01:29:26 -0600

    moved git file and root to inside vite app

--- README.md
+++
@@ -1,2 +0,0 @@
-# Capstone22
-This repository will hold the code for my Media, Arts, and Design Capstone project developed during Winter 2022. 
--- capstonesandbox/sndbx/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SndbxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sndbx'
