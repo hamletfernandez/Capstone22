db9df2ef34d5bfb7afa6efb1b3e7aec8d0ec7aee
Imagineham <hamletfernandez57@gmail.com>
2022-03-17 12:37:21 -0500

    deleted import style

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js';
